52919f89b2c624e88e9ee6da07e4cc28594f8d18
H8 <reczek.krzysztof@gmail.com>
2016-08-29 00:31:29 +0200

    Assignment2: done and passed

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -19,31 +19,43 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
+  def singletonSet(elem: Int): Set = new Set {
+    override def apply(v: Int): Boolean = v == elem
+  }
   
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
+  def union(s: Set, t: Set): Set = new Set {
+    override def apply(v: Int): Boolean = s(v) || t(v)
+  }
+
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
+  def intersect(s: Set, t: Set): Set = new Set {
+    override def apply(v: Int): Boolean = s(v) && t(v)
+  }
+
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+  def diff(s: Set, t: Set): Set = new Set {
+    override def apply(v: Int): Boolean = s(v) && !t(v)
+  }
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
+  def filter(s: Set, p: Int => Boolean): Set = new Set {
+    override def apply(v: Int): Boolean = s(v) && p(v)
+  }
   
 
   /**
@@ -54,25 +66,40 @@ object FunSets {
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
-    def forall(s: Set, p: Int => Boolean): Boolean = {
+  def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) true
+      else if (s(a) && !p(a)) false
+      else iter(a + 1)
     }
-    iter(???)
+    iter(-bound)
   }
   
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = {
+    def predicate(a: Int): Boolean = {
+      !p(a)
+    }
+    !forall(s, predicate)
+  }
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
+  def map(s: Set, f: Int => Int): Set = {
+    def iter(acc: Set, a: Int): Set = {
+      if (a > bound) acc
+      else if (s(a)) {
+        def newAcc = if (acc != null) union(acc, singletonSet(f(a))) else singletonSet(f(a))
+        iter(newAcc, a + 1)
+      }
+      else iter(acc, a + 1)
+    }
+    iter(null, -bound)
+  }
   
   /**
    * Displays the contents of a set
--- funsets/src/main/scala/funsets/Main.scala
+++ funsets/src/main/scala/funsets/Main.scala
@@ -3,4 +3,33 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  def s1 = singletonSet(1)
+  def s2 = singletonSet(2)
+  def s3 = singletonSet(3)
+
+  println("set s1")
+  printSet(s1)
+
+  def u1 = union(s1, s2)
+  println("union u1")
+  printSet(u1)
+
+  def u2 = union(u1, s3)
+  println("union u2")
+  printSet(u2)
+
+  def p1(x: Int): Boolean = x > 1
+  def f1 = filter(u2, p1)
+
+  println("filtered f1")
+  printSet(f1)
+
+  def p2(x: Int): Boolean = x > 0
+  println(forall(u2, p2))
+
+  def p3(x: Int): Boolean = x == 3
+  println(exists(u2, p3))
+
+  def p4(x: Int): Int = x * x
+  printSet(map(u2, p4))
 }
